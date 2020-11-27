099765eb18c6e3e8800b40b592228ae94c62658b
ewanpandelus <ewanpandelus@hotmail.com>
2020-11-27 19:22:30 +0000

    second commit

--- src/ProfScrapeLib.hs
+++ src/ProfScrapeLib.hs
@@ -3,6 +3,27 @@
 module ProfScrapeLib
     (  numProfessors
     ) where
-
+import Text.HTML.Scalpel
+import Data.List
 numProfessors :: String -> IO (Maybe Int)
-numProfessors x = return Nothing
+numProfessors [] = return Nothing
+numProfessors department = do
+   res0 <- scrapeURL ("https://www.gla.ac.uk/schools/" ++ department ++ "/staff") (scrapeSchool "research-teaching")
+   res1 <- scrapeURL ("https://www.gla.ac.uk/schools/" ++ department ++ "/staff") (scrapeSchool "professional-administrative-support")
+   res2 <- scrapeURL ("https://www.gla.ac.uk/schools/" ++ department ++ "/staff") (scrapeSchool "affiliate")
+   sumResult res0 res1 res2 
+      
+sumResult:: Int->Int->Int->Int
+sumResult a b c = a+b+c
+
+
+scrapeSchool :: String -> Scraper String String
+scrapeSchool school = chroot ("div" @: ["id" @=school]) scraperATags
+
+scraperATags :: String -> Int 
+scraperATags = do
+   str <- texts "a"
+   sum map countProfessors str 
+  
+countProfessors :: String -> Int
+countProfessors str = length (filter (\x -> x == "Professor") str)
