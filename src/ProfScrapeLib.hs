0fb1ec59a38c82c6916b32859d9112ceb37551ea
ewanpandelus <ewanpandelus@hotmail.com>
2020-11-28 17:53:31 +0000

    Just 4

--- src/ProfScrapeLib.hs
+++ src/ProfScrapeLib.hs
@@ -6,15 +6,16 @@ module ProfScrapeLib
 import Data.Maybe
 import Text.HTML.Scalpel
 
-numProfessors :: String -> IO (Maybe String)
+numProfessors :: String -> IO (Maybe Int)
 numProfessors [] = return Nothing 
 numProfessors department = do 
    res0 <- scrapeURL "https://www.gla.ac.uk/schools/chemistry/staff/" scrapeComic
-   return res0  
+   return res0
 
-scrapeComic :: Scraper String String
+scrapeComic :: Scraper String Int
 scrapeComic = 
   chroot ("ul" @: ["id" @= "research-teachinglist"]) scrapeTitle
 
-scrapeTitle :: Scraper String String
-scrapeTitle = text "a"
+scrapeTitle :: Scraper String Int
+scrapeTitle = do
+   return 4
