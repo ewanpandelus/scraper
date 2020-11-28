2212364ffae679b620f45f382b016a680437f376
ewanpandelus <ewanpandelus@hotmail.com>
2020-11-28 17:18:33 +0000

    Professor Dave

--- src/ProfScrapeLib.hs
+++ src/ProfScrapeLib.hs
@@ -9,12 +9,12 @@ import Text.HTML.Scalpel
 numProfessors :: String -> IO (Maybe String)
 numProfessors [] = return Nothing 
 numProfessors department = do 
-   res0 <- scrapeURL "http://xkcd.com" scrapeComic
+   res0 <- scrapeURL "https://www.gla.ac.uk/schools/chemistry/staff/" scrapeComic
    return res0  
 
 scrapeComic :: Scraper String String
 scrapeComic = 
-  chroot ("div" @: ["id" @= "comic"]) scrapeTitle
+  chroot ("ul" @: ["id" @= "research-teachinglist"]) scrapeTitle
 
 scrapeTitle :: Scraper String String
-scrapeTitle = (attr "title" "img")
+scrapeTitle = text "a"
