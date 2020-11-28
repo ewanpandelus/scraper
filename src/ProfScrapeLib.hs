c35e064909107c92a392ec8cb7b4787418dd77c9
ewanpandelus <ewanpandelus@hotmail.com>
2020-11-28 16:48:15 +0000

    Somewhat working code

--- src/ProfScrapeLib.hs
+++ src/ProfScrapeLib.hs
@@ -3,27 +3,18 @@
 module ProfScrapeLib
     (  numProfessors
     ) where
+import Data.Maybe
 import Text.HTML.Scalpel
-import Data.List
-numProfessors :: String -> IO (Maybe Int)
-numProfessors [] = return Nothing
-numProfessors department = do
-   res0 <- scrapeURL ("https://www.gla.ac.uk/schools/" ++ department ++ "/staff") (scrapeSchool "research-teaching")
-   res1 <- scrapeURL ("https://www.gla.ac.uk/schools/" ++ department ++ "/staff") (scrapeSchool "professional-administrative-support")
-   res2 <- scrapeURL ("https://www.gla.ac.uk/schools/" ++ department ++ "/staff") (scrapeSchool "affiliate")
-   sumResult res0 res1 res2 
-      
-sumResult:: Int->Int->Int->Int
-sumResult a b c = a+b+c
 
+numProfessors :: String -> IO (Maybe String)
+numProfessors [] = return Nothing 
+numProfessors department = do 
+   res0 <- scrapeURL "http://xkcd.com" scrapeComic
+   return res0  
 
-scrapeSchool :: String -> Scraper String String
-scrapeSchool school = chroot ("div" @: ["id" @=school]) scraperATags
+scrapeComic :: Scraper String String
+scrapeComic = 
+  chroot ("div" @: ["id" @= "comic"]) scrapeTitle
 
-scraperATags :: String -> Int 
-scraperATags = do
-   str <- texts "a"
-   sum map countProfessors str 
-  
-countProfessors :: String -> Int
-countProfessors str = length (filter (\x -> x == "Professor") str)
+scrapeTitle :: Scraper String String
+scrapeTitle = (attr "title" "img")
